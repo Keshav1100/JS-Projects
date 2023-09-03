fddc978e47cb3efdd02159cd3295fc1db8333068
Keshav Agarwal <keshav1105ag@gmail.com>
2023-09-03 15:11:24 +0530

    completed project 8

---
+++ Project_8/index.js
@@ -0,0 +1,17 @@
+let qrImgBox = document.getElementById("qr-img-box");
+let qrImg = document.getElementById("qr-img");
+async function genQr() {
+  let textUrl = document.getElementById("text-url");
+  if (textUrl.value !== "") {
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${textUrl.value}`;
+    qrImgBox.style.display = "block";
+  } else {
+    qrImgBox.style.display = "none";
+    genQrCodeBtn.classList.add("shake-error");
+    setTimeout(() => {
+      genQrCodeBtn.classList.remove("shake-error");
+    }, 2000);
+  }
+}
+let genQrCodeBtn = document.getElementById("gen-qr-code");
+genQrCodeBtn.addEventListener("click", genQr);
