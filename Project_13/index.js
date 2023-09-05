845e142bf2ed2c89d4e30122312b7e136d40a1a1
Keshav Agarwal <keshav1105ag@gmail.com>
2023-09-05 14:53:24 +0530

    Completed project 13

--- Project_13/index.js
+++ Project_13/index.js
@@ -1,13 +1,15 @@
 const submitBtn = document.getElementById("submit-btn");
 const popupOkBtn = document.getElementById("popup-ok-btn");
 const popupContainer = document.querySelector(".popup-container");
+// Function to show popup
 function showPopup() {
-  popupContainer.classList.remove("popup-hide");
-  popupContainer.classList.add("popup-show");
+  popupContainer.style.transform = "translateX(-50%) translateY(-50%) scale(1)";
+  popupContainer.style.visibility = "visible";
 }
+// Function to hide popup
 function hidePopup() {
-    popupContainer.classList.add("popup-hide");
-    popupContainer.classList.remove("popup-show");
+  popupContainer.style.transform = "translateX(-50%) translateY(-50%) scale(0)";
+  popupContainer.style.visibility = "hidden";
 }
 submitBtn.addEventListener("click", showPopup);
 popupOkBtn.addEventListener("click", hidePopup);
